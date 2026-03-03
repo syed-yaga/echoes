1d48579dec2dcbb8947739d2ef7a1202adf014e2
Syed Saqib <soharwardiandco27@gmail.com>
2026-03-03 16:56:12 +0530

    some little changes

--- api/index.ts
+++ api/index.ts
@@ -37,7 +37,7 @@ app.use("/api/auth", authrouter);
 app.use("/api/post", postrouter);
 app.use("/api/comment", commentrouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
